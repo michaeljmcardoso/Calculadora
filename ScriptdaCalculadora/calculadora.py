7835dd16331b5df78ad1d7b68fa4c97aa62f64c2
Michael <michaelmiranda38@yahoo.com.br>
2022-02-19 23:03:51 -0300

    Copiei o script da calculadora

---
+++ ScriptdaCalculadora/calculadora.py
@@ -0,0 +1,56 @@
+def bemvindo():
+    print('''Bem-vindo à Calculadora''')
+
+def calcular():
+    operação = input('''
+Digite a operação matemática que deseja realizar:
++ para adição
+- para subtração
+* para multiplicação
+/ para divisão
+% para resto da divisão
+** para potenciação
+''')
+
+    numero_1 = int(input('Digite o primeiro número: '))
+    numero_2 = int(input('Digite o segundo número: '))
+
+    if operação == '+':
+        print('{} + {} = '.format(numero_1, numero_2))
+        print(numero_1 + numero_2)
+    elif operação == '-':
+        print('{} - {} = '.format(numero_1, numero_2))
+        print(numero_1 - numero_2)
+    elif operação == '*':
+        print('{} * {} = '.format(numero_1, numero_2))
+        print(numero_1 * numero_2)
+    elif operação == '/':
+        print('{} / {} '.format(numero_1, numero_2))
+        print(numero_1 / numero_2)
+    elif operação == '%':
+        print('{} % {} '.format(numero_1, numero_2))
+        print(numero_1 % numero_2)
+    elif operação == '**':
+        print('{} ** {} '.format(numero_1, numero_2))
+        print(numero_1 ** numero_2)
+    else:
+        print('Você não digitou um operador válido, por favor execute o programa de novo! ')
+    # Adicionar novamente() função para calcular() função
+    novamente()
+
+def novamente():
+    calcule_novamente = input('''
+Você quer calcular novamente?
+Por favor digite S para SIM ou N para NÃO.
+''')
+    # Se o usuário digitar Y, execute a função calcular()
+    if calcule_novamente.upper() == 'S':
+        calcular()
+    # Se o usúario digitar N, diga até mais para o usuário e feche o programa
+    elif calcule_novamente.upper() == 'N':
+        print('Até mais! ')
+    else:
+        novamente()
+
+bemvindo()
+calcular()
